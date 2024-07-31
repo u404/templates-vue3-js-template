ecf92f1a02a065375109fde63ce4bce587926476
张光辉 <ltxwy@hotmail.com>
2024-07-31 17:20:00 +0800

    feat: env 多环境配置

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, import.meta.url, '')
+
+  return {
+    base: env.BASE_URL || '',
+    server: {
+      proxy: {
+        '/eloan': {
+          target: 'http://10.0.0.69:8081/',
+          changeOrigin: true
+          // rewrite: (path) => path.replace(/^\/urule/, '')
+        }
+      }
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
