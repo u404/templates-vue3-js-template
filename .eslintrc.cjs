1446351ed5ceeb6a7c55eee61c0ba614f22f847b
张光辉 <ltxwy@hotmail.com>
2024-07-31 17:32:55 +0800

    feat: eslint prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,19 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier/skip-formatting'],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-empty': 'off',
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'warn',
+    'vue/no-unused-components': 'warn',
+    'vue/require-v-for-key': 'warn',
+    'vue/comment-directive': 'off'
   }
 }
